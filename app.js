3891f6fc617c00eccabe5a7e7ab85ffd36fecec4
Sargam-Agarwal <sargam123india@gmail.com>
2022-09-01 11:17:54 +0530

    saved the data in the database successfully(in form of strings)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const File = require('./models/file.js');
 
 const app = express();
 
-const dbURI = 'mongodb+srv://dummyUser:password1234@files.ntdja5q.mongodb.net/?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://dummyUser:password1234@files.ntdja5q.mongodb.net/UploadedFiles?retryWrites=true&w=majority';
 
 const connectionParams = {
     useNewUrlParser: true,
@@ -17,9 +18,18 @@ mongoose.connect(dbURI, connectionParams).then(() => {
 
 app.listen(3000);
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.sendFile('./views/add_new_file.html', { root: __dirname });
 })
+
+app.post('/files', (req, res) => {
+    console.log(req.body);
+    const file = new File(req.body);
+
+    file.save().then(() => console.log("Saved Successfully!")).catch(err => console.log(err));
+});
 
 app.use((req, res) => {
     res.status(404).sendFile('./views/404.html', { root: __dirname });
---
+++ models/file.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fileSchema = new Schema({
+    fileName: {
+        type: String,
+        required: true,
+    },
+    version: {
+        type: String,
+        required: true,
+    },
+    fileURL: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const File = mongoose.model('FileCollectionssssss', fileSchema);
+
+module.exports = File;
